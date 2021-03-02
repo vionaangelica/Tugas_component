54948ee6eb4bb57dc438bc4ba05744c5f891ada9
vionaangelica <c14180157@john.petra.ac.id>
2021-03-02 20:11:53 +0700

    4 files modified

--- src/app/global-var.service.ts
+++ src/app/global-var.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class GlobalVarService {
@@ -10,69 +10,68 @@ export class GlobalVarService {
   private countFav = 0;
   private count = 0;
 
-  constructor() { }
-  public getJudul(){
+  constructor() {}
+  public getJudul() {
     return this.dataJudul;
   }
 
-  public getFav(){
+  public getFav() {
     return this.judulFav;
   }
 
-  public getIndex(judul : String){
-    var i : number = 0;
-    for(i=0;i<this.count;i++){
-      if (this.dataJudul[i] == judul){
+  public getIndex(judul: String) {
+    var i: number = 0;
+    for (i = 0; i < this.count; i++) {
+      if (this.dataJudul[i] == judul) {
         return i;
       }
     }
   }
 
-  public getIsi(judul : String){
+  public getIsi(judul: String) {
     var index = this.getIndex(judul);
     return this.dataNotes[index];
   }
-  public getTanggal(judul : String){
+  public getTanggal(judul: String) {
     var index = this.getIndex(judul);
     return this.dataTanggal[index];
   }
 
-  public getCount(){
+  public getCount() {
     return this.count;
   }
 
-  public getCountFav(){
+  public getCountFav() {
     return this.countFav;
   }
 
-  public getDicari(){
+  public getDicari() {
     return this.judulDicari;
   }
 
-  public incCount(){
-    this.count+=1;
+  public incCount() {
+    this.count += 1;
   }
 
-   public incCountFav(){
-    this.countFav+=1;
+  public incCountFav() {
+    this.countFav += 1;
   }
 
-  public setJudul(judul : String){
+  public setJudul(judul: String) {
     this.dataJudul.push(judul);
   }
-  public setIsi(isi : String){
+  public setIsi(isi: String) {
     this.dataNotes.push(isi);
   }
-  public setTanggal(tgl : String){
+  public setTanggal(tgl: String) {
     this.dataTanggal.push(tgl);
   }
 
-  public setDicari(cari : String){
+  public setDicari(cari: String) {
     this.judulDicari = cari;
   }
 
-  public setFavorite(judul : String){
+  public setFavorite(judul: String) {
     this.judulFav.push(judul);
   }
-
 }
